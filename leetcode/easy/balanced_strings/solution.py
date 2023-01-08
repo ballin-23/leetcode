ff1c7ae45e7c33a28a8cea1c7e2b13101ea1470c
ballin-23 <ballincode@gmail.com>
2023-01-08 01:42:12 -0700

    modified solution

--- leetcode/easy/balanced_strings/solution.py
+++ leetcode/easy/balanced_strings/solution.py
@@ -1,26 +1,15 @@
+def balance_string(s):
+    count = 0
+    char_dictionary = {"R":0, "L":0}
+
+    for char in s:
+        if char == "R":
+            char_dictionary["R"] += 1
+        else:
+            char_dictionary["L"] += 1
+        if char_dictionary["R"] == char_dictionary["L"]:
+            count += 1
+    print(count)
+    return count
 input = "RLRRRLLRLL"
-
-count = 0
-char_dictionary = {"R":0, "L":0}
-
-for char in input:
-    if char == "R":
-        if char_dictionary["L"] == 0:
-            char_dictionary["R"] += 1
-        else:
-            char_dictionary["R"] += 1
-            if char_dictionary["R"] == char_dictionary["L"]:
-                count += 1
-                char_dictionary["R"] = 0
-                char_dictionary["L"] = 0
-    else:
-        if char_dictionary["R"] == 0:
-            char_dictionary["L"] += 1
-        else:
-            char_dictionary["L"] += 1
-            if char_dictionary["R"] == char_dictionary["L"]:
-                count += 1
-                char_dictionary["R"] = 0
-                char_dictionary["L"] = 0
-
-print(count)
+balance_string(input)
