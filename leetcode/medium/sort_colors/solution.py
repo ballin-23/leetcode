7f83d3d7bd7be5367ede45ee8871dcdda55b0e4a
ballin-23 <ballincode@gmail.com>
2023-02-09 00:14:01 -0700

    solved

---
+++ leetcode/medium/sort_colors/solution.py
@@ -0,0 +1,21 @@
+input = [2,0,2,1,1,0]
+
+def sort_colors(nums):
+    color_dictionary = {}
+    for i in range(len(nums)):
+        if nums[i] in color_dictionary.keys():
+            color_dictionary[nums[i]].append(i)
+        else:
+            color_dictionary.update({nums[i]: [i]})
+    sorted_color_dictionary = dict(sorted(color_dictionary.items(), key=lambda x:x[0]))
+    counter = 0
+    for num in sorted_color_dictionary:
+        indices = sorted_color_dictionary[num]
+        for index in indices:
+            nums[counter] = num
+            counter += 1
+    print(nums)
+
+
+
+sort_colors(input)
