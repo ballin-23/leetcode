fdc8d57e85eb32700ac962bd2dc4ed307a7e69c1
ballin-23 <ballincode@gmail.com>
2023-03-18 10:45:55 -0600

    memoized fibonaci sequence

--- leetcode/easy/fibonaci/solution.py
+++ leetcode/easy/fibonaci/solution.py
@@ -1,10 +1,17 @@
-def fib(n):
+def fib(n, nums):
+    result = 0
+    if nums[n] != None:
+        print("hit")
+        return nums[n]
     # base case
-    if n == 1 or n == 0:
-        return 1
+    if n == 1 or n == 2:
+        nums[n] = 1
+        result =  1
     else:
-        return fib(n-1) + fib(n-2)
+        result = fib(n-1, nums) + fib(n-2, nums)
+        nums[n] = result
+    return result
 
-
-x = fib(3)
+arr = [None] * 8
+x = fib(7, arr)
 print(x)
