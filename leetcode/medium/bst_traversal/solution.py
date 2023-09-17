a737e64a3d6e8bcbff07bc1637c6d4ec1e5fd532
ballin-23 <ballincode@gmail.com>
2023-09-17 13:44:53 -0600

    understanding bst traversals better now

---
+++ leetcode/medium/bst_traversal/solution.py
@@ -0,0 +1,28 @@
+def inOrderTraverse(tree, array):
+    # Write your code here.
+    if tree.left:
+        inOrderTraverse(tree.left, array)
+    array.append(tree.value)
+    if tree.right:
+        inOrderTraverse(tree.right, array)
+    return array
+
+
+def preOrderTraverse(tree, array):
+    # Write your code here.
+    array.append(tree.value)
+    if tree.left:
+        preOrderTraverse(tree.left, array)
+    if tree.right:
+        preOrderTraverse(tree.right, array)
+    return array
+
+
+def postOrderTraverse(tree, array):
+    # Write your code here.
+    if tree.left:
+        postOrderTraverse(tree.left, array)
+    if tree.right:
+        postOrderTraverse(tree.right, array)
+    array.append(tree.value)
+    return array
