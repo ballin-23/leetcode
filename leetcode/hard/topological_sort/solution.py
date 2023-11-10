89c704bc3ee326b5585826ccd07956978d4c33e2
ballin-23 <ballincode@gmail.com>
2023-11-09 23:30:50 -0700

    create directed acyclic graph which i can run dfs on

---
+++ leetcode/hard/topological_sort/solution.py
@@ -0,0 +1,30 @@
+def topologicalSort(jobs, deps):
+    # create dag
+    graph = create_graph(jobs, deps)
+    # dfs
+    dfs(jobs[0], graph, [])
+    # return something
+
+def create_graph(jobs, deps):
+    graph = {}
+    for job in jobs:
+        arr = []
+        for dep in deps:
+            if dep[0] == job:
+                arr.append(dep[1])
+        graph[job] = arr
+    print(graph)
+    return graph
+
+def dfs(start, graph, explored):
+    children = graph[start]
+    print("children: ", children, " of: ", start, "--- explored: ", explored)
+    for child in children:
+        if child not in explored:
+            explored.append(child)
+            dfs(child, graph, explored)
+
+jobs = [1, 2, 3, 4]
+deps = [[1, 2],[1, 3],[3, 2],[4, 2],[4, 3]]
+
+topologicalSort(jobs, deps)
