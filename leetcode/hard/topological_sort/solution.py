b719a10edc6820da06c38e4c339722b662ad56f3
ballin-23 <ballincode@gmail.com>
2023-11-10 00:13:28 -0700

    working but need to detect cycle

--- leetcode/hard/topological_sort/solution.py
+++ leetcode/hard/topological_sort/solution.py
@@ -2,8 +2,13 @@ def topologicalSort(jobs, deps):
     # create dag
     graph = create_graph(jobs, deps)
     # dfs
-    dfs(jobs[0], graph, [])
+    explored = []
+    stack = []
+    for job in jobs:
+        dfs(job, graph, explored, stack)
     # return something
+    stack.reverse()
+    print(stack)
 
 def create_graph(jobs, deps):
     graph = {}
@@ -16,13 +21,22 @@ def create_graph(jobs, deps):
     print(graph)
     return graph
 
-def dfs(start, graph, explored):
+def dfs(start, graph, explored, stack):
     children = graph[start]
     print("children: ", children, " of: ", start, "--- explored: ", explored)
     for child in children:
         if child not in explored:
             explored.append(child)
-            dfs(child, graph, explored)
+            dfs(child, graph, explored, stack)
+            print("back from dfs of this node: ", child, " this was the start: ", start)
+    if start not in stack:
+        stack.append(start)
+
+def intersection(children, explored):
+    for child in children:
+        if child in explored:
+            return True
+    return False
 
 jobs = [1, 2, 3, 4]
 deps = [[1, 2],[1, 3],[3, 2],[4, 2],[4, 3]]
